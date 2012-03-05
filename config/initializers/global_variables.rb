441a602db8b61b6be9785aa942afb7765afecdcf
Ada Yu <ada@Administrators-MacBook-Pro-4.local>
2012-03-04 22:30:57 -0800

    first commit

---
+++ config/initializers/global_variables.rb
@@ -0,0 +1,23 @@
+# Change globals to match the proper value for your site
+
+SITE_NAME = 'Baynote Mobile App'
+SITE = Rails.env == 'production' ? 'baynote.herokuapp.com' : 'localhost:3000'
+CN = 'beaucoup'
+CC = 'bea'
+WEB_TITLE = "Mobile Website Demo"
+
+#Guides to call (configure in Insights
+HOMEREC = 'HomeRec'
+PDPREC = 'PDPRec'
+CARTREC = 'CartRec'
+
+# What to display on page based on XML Results
+TITLE = 'Title'
+PRICE = 'Price'
+PRODUCT_ID = 'ProductID'
+DESCRIPTION = ''
+THUMBNAIL = 'ThumbUrl'
+CURRENCY = '$'
+
+BN_SERVER = 'http://pm01.baynote.net'
+
